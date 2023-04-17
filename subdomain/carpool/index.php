f2cb2b213a9a037923a87a4732a238ead03a94b6
lanceskylerr <caolb@students.nu-baliwag.edu.ph>
2023-04-18 00:40:49 +0800

    Revised email

--- subdomain/carpool/index.php
+++ subdomain/carpool/index.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CARPOOOL!</title>
+  <title>Register Form</title>
 </head>
 <body>
-    <H1>CARPOOL IS DABEST!</H1>
+ <form action="insert.php" method="POST">
+  <table>
+   <tr>
+    <td>Name :</td>
+    <td><input type="text" name="username" required></td>
+   </tr>
+   <tr>
+    <td>Password :</td>
+    <td><input type="password" name="password" required></td>
+   </tr>
+   
+   <tr>
+    <td>Email :</td>
+    <td><input type="email" name="email" required></td>
+   </tr> 
+   <tr>
+    <td>Phone no :</td>
+    <td>
+     </select>
+     <input type="phone" name="phone" required>
+    </td>
+   </tr>
+   <tr>
+    <td><input type="submit" value="Submit" name="submit"></td>
+    <td><input type="submit" value="Registered List" name="list"></td>
+   </tr>
+  </table>
+ </form>
 </body>
 </html>
